0d64d9e60bae08dce93b498fe19d747fbe633a9c
toastythetoaster <grwdragon@gmail.com>
2020-06-23 21:53:05 -0400

    Update attempt #1

--- data-updates.lua
+++ data-updates.lua
@@ -14,7 +14,9 @@ if settings.startup["p4a-on-recipes"].value then
 end
 
 if settings.startup["p4a-on-beacons"].value then
-    for k, v in pairs(data.raw.beacon) do
-        table.insert(v.allowed_effects, "productivity")
+    table.insert(data.raw.beacon["basic-beacon"].allowed_effects, "productivity")
+    if mods["space-exploration"] then
+        table.insert(data.raw.beacon["se-wide-beacon"].allowed_effects, "productivity")
+        table.insert(data.raw.beacon["se-wide-beacon-2"].allowed_effects, "productivity")
     end
 end
